725702fcb7531d52c8c7cd02e1404d3af9626c6a
Tom Wai-Hong Tam <waihong@chromium.org>
2011-07-06 17:59:54 +0800

    Remove un-used function transmit_cmd in tpm driver to fix warning.
    BUG=none
    TEST=enable tpm and build without warning
    
    Change-Id: I638b7f097e368ae1ec8e48b7205c0275a78e5600
    Reviewed-on: http://gerrit.chromium.org/gerrit/3672
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- drivers/tpm/slb9635_i2c/tpm.c
+++ drivers/tpm/slb9635_i2c/tpm.c
@@ -421,22 +421,6 @@ enum tpm_sub_capabilities {
 	TPM_CAP_PROP_TIS_DURATION = cpu_to_be32(0x120),
 };
 
-static ssize_t transmit_cmd(struct tpm_chip *chip, struct tpm_cmd_t *cmd,
-			    int len, const char *desc)
-{
-	int err;
-
-	len = tpm_transmit((u8 *) cmd, len);
-	if (len < 0)
-		return len;
-	if (len == TPM_ERROR_SIZE) {
-		err = be32_to_cpu(cmd->header.out.return_code);
-		dev_dbg(chip->dev, "A TPM error (%d) occurred %s\n", err, desc);
-		return err;
-	}
-	return 0;
-}
-
 struct tpm_chip *tpm_register_hardware(const struct tpm_vendor_specific *entry)
 {
 	struct tpm_chip *chip;
