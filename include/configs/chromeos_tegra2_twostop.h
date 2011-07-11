d494d49e6a69df0ad046410544c89eb5082ab032
Che-Liang Chiou <clchiou@chromium.org>
2011-07-11 17:14:37 +0800

    CHROMIUM: twostop: add twostop skeleton
    BUG=chromium-os:17424
    TEST=build cleanly
    
    Change-Id: I7e4b0051b0a0e594035d4d0cb64441981433d145
    Reviewed-on: http://gerrit.chromium.org/gerrit/3851
    Tested-by: Che-Liang Chiou <clchiou@chromium.org>
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>

---
+++ include/configs/chromeos_tegra2_twostop.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __configs_chromeos_tegra2_twostop_h__
+#define __configs_chromeos_tegra2_twostop_h__
+
+/* TODO This should be renamed to chromeos_tegra2_common.h? */
+#include <configs/chromeos_seaboard_common.h>
+
+/* entry point of twostop firmware */
+#define CONFIG_CMD_VBOOT_TWOSTOP
+
+#endif /* __configs_chromeos_tegra2_twostop_h__ */
