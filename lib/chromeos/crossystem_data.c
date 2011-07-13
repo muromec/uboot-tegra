a878e2274a1684e26e5e7a856c18618e7698a746
Olof Johansson <olofj@chromium.org>
2011-07-13 14:25:08 -0700

    CHROMIUM: use more conforming property names
    BUG=none
    TEST=none
    
    Change-Id: Ie6daa57f3a3fff6a35217e1bb7c1c0c219b688ca
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/4050
    Reviewed-by: Rong Chang <rongchang@chromium.org>

--- lib/chromeos/crossystem_data.c
+++ lib/chromeos/crossystem_data.c
@@ -136,40 +136,76 @@ int crossystem_data_embed_into_fdt(crossystem_data_t *cdata, void *fdt,
 		return 1;
 	}
 
-#define set_scalar_prop(f) \
-	err |= fdt_setprop_cell(fdt, nodeoffset, #f, cdata->f)
-#define set_array_prop(f) \
-	err |= fdt_setprop(fdt, nodeoffset, #f, cdata->f, sizeof(cdata->f))
-#define set_string_prop(f) \
-	err |= fdt_setprop_string(fdt, nodeoffset, #f, cdata->f)
+	fdt_setprop_string(fdt, nodeoffset, "compatible", "chromeos-firmware");
+
+#define set_scalar_prop(name, f) \
+	err |= fdt_setprop_cell(fdt, nodeoffset, name, cdata->f)
+#define set_array_prop(name, f) \
+	err |= fdt_setprop(fdt, nodeoffset, name, cdata->f, sizeof(cdata->f))
+#define set_string_prop(name, f) \
+	err |= fdt_setprop_string(fdt, nodeoffset, name, cdata->f)
+#define set_conststring_prop(name, str) \
+	err |= fdt_setprop_string(fdt, nodeoffset, name, str)
+#define set_bool_prop(name, f) \
+	if (cdata->f) err |= fdt_setprop(fdt, nodeoffset, name, NULL, 0)
 
 	err = 0;
-	set_scalar_prop(total_size);
-	set_string_prop(signature);
-	set_scalar_prop(version);
-	set_scalar_prop(nvcxt_lba);
-	set_array_prop(vbnv);
-	set_array_prop(nvcxt_cache);
-	set_scalar_prop(write_protect_sw);
-	set_scalar_prop(recovery_sw);
-	set_scalar_prop(developer_sw);
-	set_scalar_prop(gpio_port_write_protect_sw);
-	set_scalar_prop(gpio_port_recovery_sw);
-	set_scalar_prop(gpio_port_developer_sw);
-	set_scalar_prop(polarity_write_protect_sw);
-	set_scalar_prop(polarity_recovery_sw);
-	set_scalar_prop(polarity_developer_sw);
-	set_array_prop(binf);
-	set_scalar_prop(chsw);
-	set_string_prop(hwid);
-	set_string_prop(fwid);
-	set_string_prop(frid);
-	set_scalar_prop(fmap_base);
-	set_array_prop(vbshared_data);
+	set_scalar_prop("total-size", total_size);
+	set_string_prop("signature", signature);
+	set_scalar_prop("version", version);
+	set_scalar_prop("nonvolatile-context-lba", nvcxt_lba);
+	set_scalar_prop("nonvolatile-context-offset", vbnv[0]);
+	set_scalar_prop("nonvolatile-context-size", vbnv[1]);
+	set_array_prop("boot-nonvolatile-cache", nvcxt_cache);
+
+	set_bool_prop("boot-write-protect-switch", write_protect_sw);
+	set_bool_prop("boot-recovery-switch", recovery_sw);
+	set_bool_prop("boot-developer-switch", developer_sw);
+
+	set_scalar_prop("write-protect-switch-gpio", gpio_port_write_protect_sw);
+	set_scalar_prop("recovery-switch-gpio", gpio_port_recovery_sw);
+	set_scalar_prop("developer-switch-gpio", gpio_port_developer_sw);
+	set_scalar_prop("write-protect-switch-polarity", polarity_write_protect_sw);
+	set_scalar_prop("recovery-switch-polarity", polarity_recovery_sw);
+	set_scalar_prop("developer-switch-polarity", polarity_developer_sw);
+
+	set_scalar_prop("boot-reason", binf[0]);
+	switch (cdata->binf[1]) {
+	case 0:
+		set_conststring_prop("active-firmware", "recovery");
+	case 1:
+		set_conststring_prop("active-firmware", "A");
+	case 2:
+		set_conststring_prop("active-firmware", "B");
+	}
+
+	switch (cdata->binf[2]) {
+	case 0:
+		set_conststring_prop("active-ec-firmware", "RO");
+	case 1:
+		set_conststring_prop("active-ec-firmware", "RW");
+	}
+
+	switch (cdata->binf[3]) {
+	case 0:
+		set_conststring_prop("firmware-type", "recovery");
+	case 1:
+		set_conststring_prop("firmware-type", "normal");
+	case 2:
+		set_conststring_prop("firmware-type", "developer");
+	}
+	set_scalar_prop("recovery-reason", binf[4]);
+
+	set_string_prop("hardware-id", hwid);
+	set_string_prop("firmware-version", fwid);
+	set_string_prop("readonly-firmware-version", frid);
+	set_scalar_prop("fmap-offset", fmap_base);
+	set_array_prop("vboot-shared-data", vbshared_data);
 
 #undef set_scalar_prop
 #undef set_array_prop
 #undef set_string_prop
+#undef set_bool_prop
 
 	if (err)
 		VBDEBUG(PREFIX "fail to store all properties into fdt\n");
