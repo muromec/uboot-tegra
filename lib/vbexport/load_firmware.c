a9d54e25d795b11582d5b8f692ec060509f7848b
Tom Wai-Hong Tam <waihong@chromium.org>
2011-07-15 12:00:48 +0800

    CHROMIUM: Do partial load and hash firmware body to improve speed.
    Do partial load and hash can use the advantage of cache.
    
    Here is the benchmark results:
    
    Block size of each time read
          : microsecond of running VbLoadFirmware()
    512   : 487998
    1K    : 484538
    2K    : 482178
    4K    : 480991
    8K    : 480623  <- the best
    16K   : 481034
    32K   : 481111
    64K   : 481480
    128K  : 481739
    256K  : 481280
    512K  : 481377  <- original
    
    So we pick the block size as 8K.
    
    BUG=none
    TEST=build u-boot and boot it to kernel on seaboard.
    
    Change-Id: I62432c4f7d57d102f834aeb658f2c197d3fba03b
    Reviewed-on: http://gerrit.chromium.org/gerrit/4162
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- lib/vbexport/load_firmware.c
+++ lib/vbexport/load_firmware.c
@@ -17,6 +17,12 @@
 
 #define PREFIX			"load_firmware: "
 
+/*
+ * Amount of bytes we read each time we call read().
+ * See the commit log for more detail about the benchmark results.
+ */
+#define BLOCK_SIZE		(8 * 1024)
+
 VbError_t VbExHashFirmwareBody(VbCommonParams* cparams,
 			       uint32_t firmware_index)
 {
@@ -24,6 +30,8 @@ VbError_t VbExHashFirmwareBody(VbCommonParams* cparams,
 	firmware_storage_t *file = cache->file;
 	firmware_info_t *info;
 	int index;
+	uint8_t *buffer;
+	size_t offset, leftover, n;
 
 	if (firmware_index != VB_SELECT_FIRMWARE_A &&
 			firmware_index != VB_SELECT_FIRMWARE_B) {
@@ -34,13 +42,24 @@ VbError_t VbExHashFirmwareBody(VbCommonParams* cparams,
 
 	index = (firmware_index == VB_SELECT_FIRMWARE_A ? 0 : 1);
 	info = &cache->infos[index];
+	buffer = info->buffer;
+	offset = info->offset;
 
-	if (file->read(file, info->offset, info->size, info->buffer)) {
-		VbExDebug(PREFIX "fail to read firmware body: %d\n", index);
-		return 1;
+	/*
+	 * This loop feeds firmware body into VbUpdateFirmwareBodyHash.
+	 */
+	for (leftover = info->size;
+			leftover > 0;
+			leftover -= n, offset += n, buffer += n) {
+		n = min(BLOCK_SIZE, leftover);
+		if (file->read(file, offset, n, buffer)) {
+			VbExDebug(PREFIX "an error has occured "
+					"while reading firmware");
+			return 1;
+		}
+
+		VbUpdateFirmwareBodyHash(cparams, buffer, n);
 	}
-
-	VbUpdateFirmwareBodyHash(cparams, info->buffer, info->size);
 
 	return 0;
 }
